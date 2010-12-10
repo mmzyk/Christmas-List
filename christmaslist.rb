1b106083ed7e225e93527c642afed26563525b9d
Mark Mzyk <mmzyk@programmersparadox.com>
2010-12-10 07:19:12 -0500

    Refactored code for methods to be more self documenting.

--- christmaslist.rb
+++ christmaslist.rb
@@ -1,6 +1,17 @@
 # Create a Christmas given list.  Given a file with each person on a single line
 # create a list that pairs each person with another that they shoudl get a gift for.
 
+def find_pair(person)
+  total = @pairs.length
+  pick = rand(total)
+  if invalid_pair? pick,person
+    pair = @pairs.delete_at(pick)
+  else 
+    pair = find_pair(person)  
+  end  
+  pair  
+end
+
 def read_people
   persons = Array.new
   f = File.open("people.txt", "r")
@@ -10,17 +21,22 @@ def read_people
   persons
 end  
   
-def find_pair(person)
-  total = @pairs.length
-  pick = rand(total)
-  if @pairs[pick] != person
-    pair = @pairs.delete_at(pick)
-  else 
-    pair = find_pair(person)  
-  end  
-  pair  
-end  
-    
+
+def invalid_pair?(pick, person)
+  valid = true
+  if same_person? pick,person
+    valid = false
+  end
+  valid
+end      
+  
+def same_person?(pick, person) 
+  same = false
+  if @pairs[pick] == person
+    same = true
+  end
+  same  
+end   
   
 @peoples = read_people
 @pairs = @peoples.dup  
